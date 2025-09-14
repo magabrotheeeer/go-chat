9aa87820525ac8ee9333d0bd0fc832a1c20edf8a
Magomed Akhilgov <akhilgov_magomed@inbox.ru>
2025-09-14 18:40:05 +0300

    parser for config.yaml and environment variables

--- config/config.go
+++ config/config.go
@@ -3,24 +3,37 @@ package config
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Env                     string `yaml:"env"`
-	StorageConnectionString string `yaml:"storage_connection_string"`
-	HTTPServer              struct {
-		AddressHTTP     string `yaml:"addresshttp"`
-		TimeoutHTTP     string `yaml:"timeouthttp"`
-		IdleTimeOutHTTP string `yaml:"idle_timeouthttp"`
-	} `yaml:"http_server"`
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
 }
 
+type DatabaseConfig struct {
+	Host       string `yaml:"host"`
+	Port       string `yaml:"port"`
+	Name       string `yaml:"name"`
+	User       string `yaml:"-"` // из env
+	Password   string `yaml:"-"` // из env
+	Connection string `yaml:"-"` // из env
+	SSLMode    string `yaml:"ssl_mode"`
+}
+
+type ServerConfig struct {
+	Port        string        `yaml:"port"`
+	Timeout     time.Duration `yaml:"timeout"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+// MustLoad функция для загрузки конфиг файла
 func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
-		log.Fatal("CONFIG PATH is not set")
+		log.Fatal("CONFIG_PATH is not set")
 	}
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.Fatalf("file %s - does not exist", configPath)
@@ -28,6 +41,18 @@ func MustLoad() *Config {
 	var cfg Config
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
 		log.Fatalf("cannot read config: %s", err)
+	}
+	cfg.Database.User = os.Getenv("POSTGRES_USER")
+	if cfg.Database.User == "" {
+		log.Fatal("POSTGRES_USER is not set")
+	}
+	cfg.Database.Password = os.Getenv("POSTGRES_PASSWORD")
+	if cfg.Database.Password == "" {
+		log.Fatal("POSTGRES_PASSWORD is not set")
+	}
+	cfg.Database.Connection = os.Getenv("POSTGRES_CONNECTION")
+	if cfg.Database.Connection == "" {
+		log.Fatal("POSTGRES_CONNECTION is not set")
 	}
 	return &cfg
 }
