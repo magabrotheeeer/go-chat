9e2f147623dd203fdd1df1a650613423fac7cc1d
Magomed Akhilgov <akhilgov_magomed@inbox.ru>
2025-09-14 18:38:37 +0300

    separate client and hub

--- internal/chat/infrastructure/wsserver/hub.go
+++ internal/chat/infrastructure/wsserver/hub.go
@@ -1,106 +1,10 @@
 package wsserver
 
 import (
-	"context"
-	"log"
 	"sync"
-	"time"
 
-	"github.com/gorilla/websocket"
 	entities "github.com/magabrotheeeer/go-chat/internal/chat/domain/entities"
-	repositories "github.com/magabrotheeeer/go-chat/internal/chat/domain/repositories"
 )
-
-type Client struct {
-	RoomID string
-	Send   chan *entities.Message
-}
-
-const (
-	// Время ожидания записи сообщения в peer
-	writeWait = 10 * time.Second
-
-	// Время ожидания pong от peer
-	pongWait = 60 * time.Second
-
-	// Отправка ping в peer. Должно быть меньше pongWait
-	pingPeriod = (pongWait * 9) / 10
-
-	// Максимальный размер сообщения
-	maxMessageSize = 512
-)
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-// ReadPump перекачивает сообщения из WebSocket соединения в hub
-func (c *Client) ReadPump(conn *websocket.Conn, msgRepo repositories.MessageRepository, hub *Hub) {
-	defer func() {
-		hub.UnregisterClient(c)
-		conn.Close()
-	}()
-
-	conn.SetReadLimit(maxMessageSize)
-	conn.SetReadDeadline(time.Now().Add(pongWait))
-	conn.SetPongHandler(func(string) error {
-		conn.SetReadDeadline(time.Now().Add(pongWait))
-		return nil
-	})
-
-	for {
-		var msg entities.Message
-		err := conn.ReadJSON(&msg)
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
-			}
-			break
-		}
-
-		// Сохраняем сообщение в базу данных
-		msg.RoomID = c.RoomID
-		err = msgRepo.Save(context.Background(), &msg)
-		if err != nil {
-			log.Printf("Error saving message: %v", err)
-			continue
-		}
-
-		// Отправляем сообщение всем клиентам в комнате
-		hub.BroadcastMessage(&msg)
-	}
-}
-
-// WritePump перекачивает сообщения из hub в WebSocket соединение
-func (c *Client) WritePump(conn *websocket.Conn) {
-	ticker := time.NewTicker(pingPeriod)
-	defer func() {
-		ticker.Stop()
-		conn.Close()
-	}()
-
-	for {
-		select {
-		case message, ok := <-c.Send:
-			conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if !ok {
-				conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
-
-			if err := conn.WriteJSON(message); err != nil {
-				return
-			}
-
-		case <-ticker.C:
-			conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				return
-			}
-		}
-	}
-}
 
 type Hub struct {
 	mu         sync.RWMutex
